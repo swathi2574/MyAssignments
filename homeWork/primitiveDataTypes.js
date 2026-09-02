e948c949cfdddf401bb7aae82efb3d08b1b853e1
swathi2574 <swathi2574@gmail.com>
2026-09-03 02:01:38 +0530

    Add files via upload
    day 1

---
+++ homeWork/primitiveDataTypes.js
@@ -0,0 +1,23 @@
+let userName = "Swathi"
+console.log(userName); //Swathi
+console.log(typeof userName); //String
+
+let organization = 'Infosys'
+console.log(organization);
+console.log(typeof organization);
+
+let contactNumber=90254782934
+console.log(contactNumber);
+console.log(typeof contactNumber); //number
+
+let knowsAutomation = true
+console.log(knowsAutomation)
+console.log(typeof knowsAutomation); //boolean
+
+let usesPlaywright 
+console.log(usesPlaywright);
+console.log(typeof usesPlaywright);
+
+
+
+
